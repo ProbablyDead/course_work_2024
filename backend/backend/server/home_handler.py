03120f385f3e7c7189726df878d2aa45916e7dec
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-03 04:21:57 +0300

    aaaaaaaaaaaaaaa

--- backend/backend/server/home_handler.py
+++ backend/backend/server/home_handler.py
@@ -9,19 +9,19 @@ home_router = APIRouter()
 
 dir = Path(str(CLIENT_PATH))
 
-# home_router.mount("/static", StaticFiles(directory=dir / "static", html=True), "static")
-# templates = Jinja2Templates(directory=dir)
+home_router.mount("/static", StaticFiles(directory=dir / "static", html=True), "static")
+templates = Jinja2Templates(directory=dir)
 
-# @home_router.get("/")
-# async def root(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
+@home_router.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-# # Additional route to serve other assets like JS, CSS, etc.
-# @home_router.get("/{full_path:path}")
-# async def read_react_app(full_path: str):
-#     file_path = dir / full_path
-#     if Path.exists(file_path):
-#         return FileResponse(file_path)
-#     else:
-#         # Fallback to index.html for client-side routing
-#         return FileResponse(dir / 'index.html')
+# Additional route to serve other assets like JS, CSS, etc.
+@home_router.get("/{full_path:path}")
+async def read_react_app(full_path: str):
+    file_path = dir / full_path
+    if Path.exists(file_path):
+        return FileResponse(file_path)
+    else:
+        # Fallback to index.html for client-side routing
+        return FileResponse(dir / 'index.html')
