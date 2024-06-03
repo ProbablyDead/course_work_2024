5614d1bd2a9abcc8f803c25abd74c0476ef7cd33
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-03 04:21:13 +0300

    fix

--- backend/backend/server/__init__.py
+++ backend/backend/server/__init__.py
@@ -5,7 +5,7 @@ from .users_handlers import users_router
 
 app = FastAPI()
 
-app.include_router(documents_router, prefix="/documents")
-app.include_router(users_router, prefix="/user")
+app.include_router(documents_router, prefix="/api/documents")
+app.include_router(users_router, prefix="/api/user")
 app.include_router(home_router)
 
