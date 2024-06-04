72b422cff45c832be647a64a5a170634bd80af46
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-04 17:24:34 +0300

    add port env

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -5,5 +5,4 @@ load_dotenv()
 
 MONGO_DB_PATH = f"mongodb://{os.getenv("MONGO_USER")}:{os.getenv("MONGO_PASSWORD")}@mongodb:27017"
 CLIENT_PATH = os.getenv("CLIENT_PATH")
-
-PORT = 80
+PORT = os.getenv("PORT")
