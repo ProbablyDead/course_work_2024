20aabf82347b11856d90e20d95ce5e24f110cb9c
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-06 02:25:52 +0300

    add cors

--- backend/backend/server/__init__.py
+++ backend/backend/server/__init__.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from .home_handler import home_router
 from .documents_handlers import documents_router
 from .users_handlers import users_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+        CORSMiddleware, 
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"])
 
 app.include_router(documents_router, prefix="/api/documents")
 app.include_router(users_router, prefix="/api/user")
