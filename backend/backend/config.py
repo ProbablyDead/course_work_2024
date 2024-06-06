833247acbfda37e0ccfe7e1746e80fb432b85bac
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-06 18:32:37 +0300

    fix

--- backend/backend/config.py
+++ backend/backend/config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGO_DB_PATH = f"mongodb://{os.getenv("MONGO_USER")}:{os.getenv("MONGO_PASSWORD")}@mongodb"
+MONGO_DB_PATH = f"mongodb://{os.getenv("MONGO_USER")}:{os.getenv("MONGO_PASSWORD")}@mongodb:27017"
 CLIENT_PATH = os.getenv("CLIENT_PATH")
 PORT = os.getenv("PORT")
