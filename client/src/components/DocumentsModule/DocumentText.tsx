76fe2b2aab25f0af1fca81661da152ea68076901
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2024-06-11 18:54:48 +0300

    add rendering html

---
+++ client/src/components/DocumentsModule/DocumentText.tsx
@@ -0,0 +1,14 @@
+import React, { forwardRef, Ref } from "react";
+
+interface DocumentTextProps {
+    text: string;
+    isEditable: boolean;
+};
+
+const DocumentText = forwardRef<HTMLDivElement, DocumentTextProps>(({text, isEditable}, ref: Ref<HTMLDivElement>) => {
+    return (
+    <div ref={ref} contentEditable={isEditable} dangerouslySetInnerHTML={{__html: text}}></div>
+    );
+});
+
+export default DocumentText;
